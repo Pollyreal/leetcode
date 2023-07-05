a630e1ac2436b8ee9631b8965b307ff20be59940
Polly Wu <pollyng@foxmail.com>
2023-07-05 09:40:41 +0800

    commit daily problem

---
+++ 2600.KItemsWithMaximumSum.cs
@@ -0,0 +1,23 @@
+public partial class Solution
+{
+    public int KItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k)
+    {
+        var left = k - numOnes;
+        if (left <= 0)
+        {
+            return k;
+        }
+        else
+        {
+            left = left - numZeros;
+            if (left <= 0)
+            {
+                return numOnes;
+            }
+            else
+            {
+                return numOnes - left;
+            }
+        }
+    }
+}
