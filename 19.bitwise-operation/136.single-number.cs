0cc4f2d15b4511dfa69cb2c5a5944f57c7e88561
Polly Wu <pollyng@foxmail.com>
2023-07-12 13:56:00 +0800

    commit 136 by bitwise operation

---
+++ 19.bitwise-operation/136.single-number.cs
@@ -0,0 +1,17 @@
+public partial class BitwiseOperationSolution
+{
+    // 给你一个 非空 整数数组 nums，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+    // 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+    public int SingleNumber(int[] nums)
+    {
+        int res = nums[0];
+        if (nums.Length > 1)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                res ^= nums[i];
+            }
+        }
+        return res;
+    }
+}
