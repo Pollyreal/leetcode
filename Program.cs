0e5f7c440ca7fd855685237e11b38dedceeae85d
Polly Wu <pollyng@foxmail.com>
2023-07-17 15:01:47 +0800

    commit notes regarding csharp features

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿// Console.WriteLine(new DailyProblemSolution().CheckOverlap(1583, -829, -112, 700, -720, 918, -399));
+﻿if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine($"Argument={arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
+// Console.WriteLine(new DailyProblemSolution().CheckOverlap(1583, -829, -112, 700, -720, 918, -399));
 // Console.WriteLine(new DailyProblemSolution().PivotInteger(1));
 // Console.WriteLine(new DailyProblemSolution().MaximumSum(new int[] { 1, -4, -5, -2, 5, 0, -1, 2 }));
 // Console.WriteLine(new DailyProblemSolution().Fib(2));
 // var res = new DailyProblemSolution().ReconstructMatrix(5, 5, new int[] { 2, 1, 2, 0, 1, 0, 1, 2, 0, 1 });
 
-var res = new DailyProblemSolution().IsCircularSentence("I like Leetcode");
-Console.WriteLine(res);
+// var res = new DailyProblemSolution().IsCircularSentence("I like Leetcode");
+// Console.WriteLine(res);
+
+Console.WriteLine(await new LearnSolution().RetrieveDocsHomePage());
+Console.WriteLine(new LearnSolution().TestPatternMatch("Hello"));
---
+++ microsoft-learn/csharp-features.cs
@@ -0,0 +1,47 @@
+/// <summary>
+/// <para>
+/// 1. 如何选择运行框架的C#版本
+/// https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/configure-language-version#defaults 
+/// </para>
+/// <para>
+/// 2. C# 代码约定
+/// https://learn.microsoft.com/zh-cn/dotnet/csharp/fundamentals/coding-style/coding-conventions
+/// </para>
+/// </summary>
+public class LearnSolution
+{
+    public async Task<int> RetrieveDocsHomePage()
+    {
+        var client = new HttpClient();
+        byte[] content = await client.GetByteArrayAsync("https://learn.microsoft.com/");
+
+        Console.WriteLine($"{nameof(RetrieveDocsHomePage)}: Finished downloading.");
+        return content.Length;
+    }
+
+    /// <summary> 
+    /// C# 8.0 features - Switch expression
+    /// <para>switch语句及switch表达式，支持的匹配模式参考以下链接
+    /// https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/operators/patterns?source=recommendations
+    /// </para></summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    public string TestPatternMatch(string parameter) =>
+    parameter switch
+    {
+        "Hey" => "hey hey!",
+        "Hello" => "Hello world!",
+        _ => "are you ok?" // _ 为弃元模式 discard pattern 来匹配任何表达式，包括 null
+    };
+
+    /// <summary>
+    /// new 运算符实例对象的简洁形式
+    /// </summary>
+    public void NewOperator()
+    {
+        // 简洁形式
+        var instance2 = new ExampleClass();
+        // since c# 9.0
+        LearnSolution instance1 = new();
+    }
+}
